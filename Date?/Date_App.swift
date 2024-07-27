8c0cb2401d7830588e8a5b4392fab60fd52055db
Atakan Başaran <basaranatakan10@gmail.com>
2024-07-27 14:18:05 +0300

    Initial Commit

---
+++ Date?/Date_App.swift
@@ -0,0 +1,17 @@
+//
+//  Date_App.swift
+//  Date?
+//
+//  Created by Atakan Başaran on 27.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Date_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
